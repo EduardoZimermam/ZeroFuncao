486dd2a287d7f1e2aade16b696397b74dd5ba388
EduardoZimermam <eduardozimerman@live.com>
2018-08-18 22:29:27 -0300

    Término das implementações das funções para encontrar as raízes.

--- ZeroFuncao.c
+++ ZeroFuncao.c
@@ -5,17 +5,71 @@
 #include "ZeroFuncao.h"
 
 double bisseccao (Polinomio p, double a, double b, double eps, int *it, double *raiz){
-	double 
+	double xm, pxm, dpxm, xmn, pxmn, dpxmn, pa, dpa;
+
+	xmn = (a + b) / 2;
+
+	do {
+		xm = xmn;
+		
+		if((calcPolinomio_rapido(p, a, &pa, &dpa) * calcPolinomio_rapido(p, xmn, &pxmn, &dpxmn)) > ZERO){
+			a = xm;
+		} else {
+			b = xm;
+		}
+
+		xmn = (a + b) / 2;
+	} while(fabs((xmn - xm) / xmn) > eps);
+
+	raiz = xmn;
+
+	return 0;
 }
 
 
 double newtonRaphson (Polinomio p, double x0, double eps, int *it, double *raiz){
+	double x, px0, dpx0;
 
+	calcPolinomio_rapido(p, x0, &px0, &dpx0);
+
+	if(dpx0 != ZERO){
+		
+		x = x0 - px0 / dpx0;
+
+		while(fabs((x - x0) / x) > eps){
+			x0 = x;
+			calcPolinomio_rapido(p, x0, &px0, &dpx0);
+			x = x0 - px0 / dpx0;
+		}
+
+		raiz = x;
+
+		return 0;
+	}
+
+	return -1;
 }
 
-
 double secante (Polinomio p, double x0, double x1, double eps, int *it, double *raiz){
+	double x, px0, dpx0, px1, dpx1;
 
+	calcPolinomio_rapido(p, x0, &px0, &dpx0);
+	calcPolinomio_rapido(p, x1, &px1, &dpx1);
+		
+	x = x1 - ((px1 * (x1 - x0)) / (px1 - px0));
+
+	while(fabs((x - x0) / x) > eps){
+		x0 = x1;
+		x1 = x;
+		calcPolinomio_rapido(p, x0, &px0, &dpx0);
+		calcPolinomio_rapido(p, x1, &px1, &dpx1);
+		
+		x = x1 - ((px1 * (x1 - x0)) / (px1 - px0));
+	}
+
+	raiz = x;
+
+	return 0;
 }
 
 
--- labZero.c
+++ labZero.c
@@ -6,21 +6,40 @@
 
 int main ()
 {
-	Polinomio p;
-	double rpx, rdpx, lpx, ldpx;
+	Polinomio p1, p2, p3, p4;
 
-	p.p = malloc(2);
+	p1.p = malloc(5);
+	p2.p = malloc(6);
+	p3.p = malloc(5);
+	p4.p = malloc(4);
 
-	p.p[0] = 3;
-	p.p[1] = 0;
-	p.p[2] = 2;
-	p.grau = 2;	
+	p1.p[0] = -15;
+	p1.p[1] = -10;
+	p1.p[2] = 3;
+	p1.p[3] = 4;
+	p1.p[4] = 2;
+	p.grau = 4;
 
-	calcPolinomio_rapido(p, 2, &rpx, &rdpx);
-	calcPolinomio_lento(p, 2, &lpx, &ldpx);
+	p2.p[0] = -24;
+	p2.p[1] = -4;
+	p2.p[2] = 22;
+	p2.p[3] = -9;
+	p2.p[4] = -2;
+	p2.p[4] = 1;
+	p.grau = 5;
 
-	printf("Rapido: Polinomio = %lf  ;  Derivada = %lf\n", rpx, rdpx);
-	printf("Lento: Polinomio = %lf  ;  Derivada = %lf\n", lpx, ldpx);
+	p3.p[0] = -30;
+	p3.p[1] = 25;
+	p3.p[2] = 4;
+	p3.p[3] = 2;
+	p3.p[4] = 3;
+	p.grau = 4;
+
+	p4.p[0] = 3;
+	p4.p[1] = -1;
+	p4.p[2] = -5;
+	p4.p[3] = 2;
+	p.grau = 3;	
 
   return 0;
 }
